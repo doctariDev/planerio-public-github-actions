2f364644968bfa78fd31bfc0ee00f98e1f91f810
Felix Brass <f.brass@planerio.de>
2024-11-26 14:38:09 +0100

    added default case for parallel deplyment

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,7 +54,7 @@ cat <<EOF
     "s3ObjectVersion": "${S3OBJECTVERSION}",
     "kafkaTopics": ${KAFKA_TOPICS_JSON},
     "uniqueId": "${UNIQUEID}",
-    "parallelDeployment": "${PARALLEL_DEPLOYMENT}"
+    "parallelDeployment": ${PARALLEL_DEPLOYMENT}
 }
 EOF
 ) > /tmp/dpl_trigger_request.json
